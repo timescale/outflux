98620042b6fb66ceb1d93ed6b28c52bf09faa65e
Blagoj Atanasovski <46002495+blagojts@users.noreply.github.com>
2019-01-10 09:09:20 +0100

    Create Intermediate Data Representation Format structures for DDL
    Structures for Data set description, column descriptions along with foreign key
    relationships.
    Unit tests writen for the functions that create the structures and also validate the
    arguments.

---
+++ idrf/idrf.go
@@ -0,0 +1,124 @@
+// Package idrf provides the structures for the Outflux Intermediate Data Reperesentation
+// Format. These structures describe data and it's schema. The package also contains
+// functions for safe initialization of the structures
+package idrf
+
+import "fmt"
+
+// DataSetInfo represents DDL description of a single data set (table, measurement) in IDRF
+type DataSetInfo struct {
+	dataSetName string
+	columns     []ColumnInfo
+}
+
+// NewDataSet creates a new instance of DataSetInfo with checked arguments
+func NewDataSet(dataSetName string, columns []ColumnInfo) (*DataSetInfo, error) {
+	if len(dataSetName) == 0 {
+		return nil, fmt.Errorf("Data set name can't be empty")
+	}
+
+	if len(columns) == 0 {
+		return nil, fmt.Errorf("Data set must have at least one column")
+	}
+
+	columnSet := make(map[string]bool)
+	for _, columnInfo := range columns {
+		if _, exists := columnSet[columnInfo.name]; exists {
+			return nil, fmt.Errorf("Duplicate column names found")
+		}
+
+		columnSet[columnInfo.name] = true
+	}
+
+	return &DataSetInfo{dataSetName, columns}, nil
+}
+
+// ColumnNamed returns the ColumnInfo for a column given it's name, or nil if no column
+// with that name exists in the data set
+func (set *DataSetInfo) ColumnNamed(columnName string) *ColumnInfo {
+	for _, column := range set.columns {
+		if columnName == column.name {
+			return &column
+		}
+	}
+
+	return nil
+}
+
+// ColumnInfo represents DDL description of a single column in IDRF
+type ColumnInfo struct {
+	name       string
+	dataType   DataType
+	foreignKey *ForeignKeyDescription
+}
+
+// NewColumnWithFK creates a new ColumnInfo with a foreign key while checking the arguments
+func NewColumnWithFK(columnName string, dataType DataType, foreignKey *ForeignKeyDescription) (*ColumnInfo, error) {
+	column, error := NewColumn(columnName, dataType)
+	if error != nil {
+		return nil, error
+	}
+
+	foreignColumn := foreignKey.dataSet.ColumnNamed(foreignKey.columnName)
+	if foreignColumn == nil {
+		return nil, fmt.Errorf("Foreign key is invalid, column not found in data set")
+	}
+
+	if foreignColumn.dataType != dataType {
+		return nil, fmt.Errorf("Foreign key remote column is of different type")
+	}
+
+	column.foreignKey = foreignKey
+	return column, nil
+}
+
+// NewColumn creates a new ColumnInfo without a foreign key while checking the arguments
+func NewColumn(columnName string, dataType DataType) (*ColumnInfo, error) {
+	if len(columnName) == 0 {
+		return nil, fmt.Errorf("Column must have a name")
+	}
+
+	return &ColumnInfo{columnName, dataType, nil}, nil
+}
+
+// DataType Supported data types in the Intermediate Data Representation Format
+type DataType int
+
+// Available values for IDRF DataType enum
+const (
+	IDRFInteger DataType = iota + 1
+	IDRFFloating
+	IDRFString
+	IDFRBoolean
+)
+
+func (d DataType) String() string {
+	switch d {
+	case IDFRBoolean:
+		return "IDRFBoolean"
+	case IDRFFloating:
+		return "IDRFFloating"
+	case IDRFInteger:
+		return "IDRFInteger"
+	case IDRFString:
+		return "IDRFString"
+	default:
+		panic("Unexpected value")
+	}
+}
+
+// ForeignKeyDescription describes a foreign key relationship to a IDRF data set's column
+type ForeignKeyDescription struct {
+	dataSet    *DataSetInfo
+	columnName string
+}
+
+// NewForeignKey creates a new instance of ForeignKeyDescription with checked arguments
+func NewForeignKey(dataSet *DataSetInfo, columnName string) (*ForeignKeyDescription, error) {
+	columnExistsInDataSet := dataSet.ColumnNamed(columnName) != nil
+	if !columnExistsInDataSet {
+		return nil, fmt.Errorf("Column %s not part of data set %s", columnName, dataSet.dataSetName)
+	}
+
+	return &ForeignKeyDescription{dataSet, columnName}, nil
+}
---
+++ idrf/idrf_test.go
@@ -0,0 +1,115 @@
+package idrf
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewForeignKey(t *testing.T) {
+	// If column doesn't exist in data set error is returned
+	dataSetNoColumns := DataSetInfo{dataSetName: "ds", columns: []ColumnInfo{}}
+	wrongColumnName := "Wrong Column"
+	goodColumnName := "Column 1"
+
+	foreignKey, error := NewForeignKey(&dataSetNoColumns, wrongColumnName)
+	if error == nil || foreignKey != nil {
+		t.Error("Error should have been returned because column is not in data set")
+	}
+
+	dataSetWithColumns := DataSetInfo{dataSetName: "ds", columns: []ColumnInfo{
+		ColumnInfo{name: goodColumnName, dataType: IDRFInteger},
+	}}
+
+	foreignKey, error = NewForeignKey(&dataSetWithColumns, wrongColumnName)
+	if error == nil || foreignKey != nil {
+		t.Error("Error should have been returned because column is not in data set")
+	}
+
+	foreignKey, error = NewForeignKey(&dataSetWithColumns, goodColumnName)
+	if error != nil || foreignKey == nil {
+		t.Error("Error should not have been returned, column is in the data set")
+	}
+}
+
+func TestNewColumn(t *testing.T) {
+	if _, err := NewColumn("", IDRFFloating); err == nil {
+		t.Error("Empty column name should not be allowed")
+	}
+
+	if col, err := NewColumn("Col Name", IDRFFloating); col == nil || err != nil {
+		t.Error("Column should have been created")
+	}
+}
+
+func TestNewColumnWithFK(t *testing.T) {
+	foreignColumn := "Col1"
+	goodDataSet := DataSetInfo{
+		dataSetName: "DSName",
+		columns:     []ColumnInfo{ColumnInfo{name: foreignColumn, dataType: IDRFFloating}},
+	}
+	goodForeignKey, err := NewForeignKey(&goodDataSet, foreignColumn)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	if _, err := NewColumnWithFK("", IDRFFloating, goodForeignKey); err == nil {
+		t.Error("Empty column name should not be allowed")
+	}
+
+	if _, err := NewColumnWithFK("Col 2", IDRFInteger, goodForeignKey); err == nil {
+		t.Error("Error because column type is not the same as the referenced foreign column")
+	}
+
+	if column, err := NewColumnWithFK("Col 2", IDRFFloating, goodForeignKey); err != nil || column == nil {
+		t.Error("Should have created the column")
+	}
+}
+
+func TestNewDataSet(t *testing.T) {
+	column, _ := NewColumn("Col 1", IDRFFloating)
+	columns := []ColumnInfo{*column}
+	if _, error := NewDataSet("", columns); error == nil {
+		t.Error("Should not be able to create a data set with an empty name")
+	}
+
+	noColumns := []ColumnInfo{}
+	if _, error := NewDataSet("Data Set", noColumns); error == nil {
+		t.Error("Should not be able to create a data set without columns")
+	}
+
+	duplicateColumns := []ColumnInfo{*column, *column}
+	if _, error := NewDataSet("data set", duplicateColumns); error == nil {
+		t.Error("Should not be able to create a data set with duplicate columns")
+	}
+
+	if dataSet, error := NewDataSet("Data Set", columns); error != nil || dataSet == nil {
+		t.Error("Data Set should have been created")
+	}
+}
+func TestColumnNamed(t *testing.T) {
+	goodColumnName := "Col 1"
+	badColumnName := "Col 2"
+
+	expectedColumnType := IDRFFloating
+
+	column, _ := NewColumn(goodColumnName, expectedColumnType)
+	columns := []ColumnInfo{*column}
+	dataSet, _ := NewDataSet("Data Set", columns)
+
+	goodColumn := dataSet.ColumnNamed(goodColumnName)
+	if goodColumn == nil {
+		t.Error("Column should have been found")
+	}
+
+	if dataSet.ColumnNamed(badColumnName) != nil {
+		t.Error("Column name should not have been found")
+	}
+
+	if goodColumn.name != goodColumnName || goodColumn.dataType != expectedColumnType {
+		t.Error(
+			fmt.Sprintf(
+				"Found column was not good. Expected: name <%s> and type <%s>. Got: name <%s> and type <%s>",
+				goodColumnName, expectedColumnType, goodColumn.name, goodColumn.dataType,
+			))
+	}
+}
