59852912843bc4352035f4aee5aef6a551c23fba
Blagoj Atanasovski <blagoj@timescale.com>
2019-02-06 23:38:37 +0100

    bind timescale docker to 5433 port

--- integrationtestutils/config.go
+++ integrationtestutils/config.go
@@ -4,7 +4,7 @@ const (
 	// InfluxHost is the default InfluxDB host to be used in integration tests
 	InfluxHost = "http://localhost:8086"
 	// TsHost is the default Timescale host to be used in integration tests
-	TsHost      = "localhost:5432"
+	TsHost      = "localhost:5433"
 	defaultPgDb = "postgres"
 	// TsUser is the default user that connects to Timescale in integration tests
 	TsUser = "postgres"
