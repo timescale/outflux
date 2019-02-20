4cc0339c11d4a991e63c4a242607a63b1f24a68f
Blagoj Atanasovski <46002495+blagojts@users.noreply.github.com>
2019-02-20 18:41:40 +0100

    Enable the usage of environment variables for specifying the influx conn (#22)
    No longer are the influx server, user and pass required flags.
    Three flags can be used INFLUX_SERVER, INFLUX_USERNAME and INFLUX_PASSWORD.
    These flags are overriden by the --input-server --input-user and
    --input-pass flags.

--- internal/flagparsers/connection_flags.go
+++ internal/flagparsers/connection_flags.go
@@ -7,22 +7,21 @@ import (
 // AddConnectionFlagsToCmd adds the flags required to connect to an Influx and Timescale database
 func AddConnectionFlagsToCmd(cmd *cobra.Command) {
 	cmd.PersistentFlags().String(
-		InputHostFlag,
-		DefaultInputHost,
+		InputServerFlag,
+		DefaultInputServer,
 		"Host of the input database, http(s)://location:port.")
 	cmd.PersistentFlags().String(
 		InputUserFlag,
 		DefaultInputUser,
-		"Username to use when connecting to the input database")
+		"Username to use when connecting to the input database. If set overrides $INFLUX_USERNAME")
 	cmd.PersistentFlags().String(
 		InputPassFlag,
 		DefaultInputPass,
-		"Password to use when connecting to the input database")
+		"Password to use when connecting to the input database. If set overrides $INFLUX_PASSWORD")
 	cmd.PersistentFlags().String(
 		OutputConnFlag,
 		DefaultOutputConn,
-		"Connection string to use to connect to the output database, i.e. 'user=a password=a host=localhost"+
-			"port=5432 dbname=test sslmode=disable'")
+		"Connection string to use to connect to the output database, overrides values in the PG environment variables")
 	cmd.PersistentFlags().String(
 		OutputSchemaFlag,
 		DefaultOutputSchema,
